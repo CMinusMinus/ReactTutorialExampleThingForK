e7ced9ee838da219e320668306fedd51f31a1b51
CMinusMinus <geocolson@gmail.com>
2017-11-14 11:50:24 -0500

    Added Submit Form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ const MyNewComponent = function(props) {
 const Title = (props) => (<div style={{
   fontWeight: 'bold', fontSize: '300px', fontStyle: 'italics', color: '#FF5579'
 }}>{props.children}</div>)
+
+const SubmitForm = ({ handleFormSubmit, formValue, handleFormChange, label, buttonStyle }) => (
+  <div style={{ postion: 'fixed', bottom: '0' }}>
+    {label}: <input style={{ height: '15px' }} type="text" onChange={handleFormChange} value={formValue} />
+    <span style={buttonStyle} onClick={handleFormSubmit}>Submit</span>
+  </div>
+)
+
+const Table = ({ values }) => (
+  <div>
+    {values.map((curr, index) => (
+      <div key={index}>
+        {curr}
+      </div>
+    ))}
+  </div>) 
  
 class App extends Component {
   // constructors are for initializing state, and binding functions
@@ -18,12 +34,16 @@ class App extends Component {
     // initialize the state
     this.state = {
       numClicks: 0, // the number of times the button has been clicked
-      textValue: '' // the current input value in the input tag
+      textValue: '', // the current input value in the input tag
+      formValue: '',
+      submittedValue: []
     };
 
     // boilerplate
     this.handleClick = this.handleClick.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
   // for button input handling
@@ -37,9 +57,19 @@ class App extends Component {
     this.setState({ textValue: e.target.value }); // keep the current input value up to date
   }
 
+  handleFormSubmit(e) {
+    const newState = this.state.submittedValue;
+    newState.push(this.state.formValue);
+    this.setState({ submittedValue: newState, formValue: '' })
+  }
+
+  handleFormChange(e) {
+    this.setState({ formValue: e.target.value });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{  }}>
         {/* This is an example of children (see const Title = (props) =>) */}
         <Title>THIS IS MY TITLE</Title>
         {/* basic props stuff (see const MyComponent = (props) =>) */}
@@ -51,7 +81,16 @@ class App extends Component {
 
         {/* text input (see handleInput, this.state) */}
         <input type="text" value={this.state.textValue} onChange={this.handleInput} />
-        <div>You said: {this.state.textValue}</div>
+        <div style={{ marginBottom: '500px' }}>You said: {this.state.textValue}</div>
+
+        <Table values={this.state.submittedValue} />
+        <SubmitForm 
+          handleFormSubmit={this.handleFormSubmit} 
+          formValue={this.state.formValue} 
+          handleFormChange={this.handleFormChange}
+          label="Label"
+          buttonStyle={{ backgroundColor: 'blue', color: 'white', padding: '5px 10px', height: '20px', borderRadius: '5px' }} 
+        />
       </div>
     );
   }
